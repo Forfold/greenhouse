700891460c68c9a5c8ccc88c3557dfc46e09f106
Forfold <nathanieljcook@outlook.com>
2026-03-07 14:20:27 -0800

    add todos to sendExceedanceNotification

--- server/src/sendExceedanceNotification.ts
+++ server/src/sendExceedanceNotification.ts
@@ -1,5 +1,6 @@
 import { LogEntry } from './saveLogRecord'
 
+// todo: need cleanup of old limit_windows
 export class SendExceedanceNotification {
   constructor (private readonly logEntry: LogEntry) {
 
@@ -15,11 +16,15 @@ export class SendExceedanceNotification {
     // todo: call db limits table, getting limit info for current log
     // todo: check log value against limit condition    // todo: if period fails condition, send notification
     if (!l.configID) {
+      // todo: check against limit_windows
+      //   if window already met (triggered_at set), don't notify
       this.sendNotification()
     }
   }
 
   private async sendNotification () {
-
+    // todo: send email to config value
+    //   notifying of limit exceeded as subject
+    //   body is subject again then log entry information
   }
 }
