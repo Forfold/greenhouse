938a16ef7b51078a7456f56417d8fb7134a174a0
Forfold <nathanieljcook@outlook.com>
2026-03-07 14:33:31 -0800

    update todos with final list

--- server/src/sendExceedanceNotification.ts
+++ server/src/sendExceedanceNotification.ts
@@ -2,9 +2,7 @@ import { LogEntry } from './saveLogRecord'
 
 // todo: need cleanup of old limit_windows
 export class SendExceedanceNotification {
-  constructor (private readonly logEntry: LogEntry) {
-
-  }
+  constructor (private readonly logEntry: LogEntry) {}
 
   async execute () {
     this.checkPeriod()
@@ -13,19 +11,22 @@ export class SendExceedanceNotification {
   private async checkPeriod () {
     const l = this.logEntry
 
-    // todo: update limit_window for current log entry
-    //   question: where is the best place to do this? here? make a limit checking class?
+    // todo: make new LimitManager() with the following:
+    //   todo: function to call db limits table, returning limit info for current log
+    //   todo: function to update limit_window for current log entry as necessary
+    //   todo: function to check log against its limit and current limit_window
+    //          (handling rate vs threshold appropriately)
 
-    // todo: call db limits table, getting limit info for current log
-    // todo: check log value against limit condition    // todo: if period fails condition, send notification
+    // todo: check log value against limitManager.checkLimit
+    // which if failed, will notify if limit condition met and
+    // limit hasn't yet been triggered for the period
     if (!l.configID) {
-      // todo: check against limit_windows
-      //   if window already met (triggered_at set), don't notify
       this.sendNotification()
     }
   }
 
   private async sendNotification () {
+    // get email address from .env -> EMAIL
     // todo: send email to config value
     //   notifying of limit exceeded as subject
     //   body is subject again then log entry information
