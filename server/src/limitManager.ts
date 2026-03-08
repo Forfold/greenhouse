abf30ced808b93a46024bfdb7d8e04ffe16918b0
Forfold <nathanieljcook@outlook.com>
2026-03-07 16:28:21 -0800

    add notification class logic using resend

---
+++ server/src/limitManager.ts
@@ -0,0 +1,35 @@
+import { Limit, LimitWindow } from './db/schema'
+import { LogEntry } from './saveLogRecord'
+
+// note: might not need all the params in the current stubs, remove as needed
+export class LimitManager {
+  constructor (private readonly logEntry: LogEntry) {}
+  // todo: need function to cleanup old limit_windows
+
+  async getLimit (log: LogEntry): Promise<Limit> {
+    // todo: get limit from db
+
+    return {
+      configID: log.configID // stub
+    } as Limit
+  }
+
+  async getLimitWindow (log: LogEntry, limit: Limit): Promise<LimitWindow> {
+    // todo: get limit window from db
+
+    return {
+      limitID: limit.id // stub
+    } as LimitWindow
+  }
+
+  // assertLog checks if the log value fails limit condition (e.g. temp > 100F within an hour)
+  async checkLogLimitAndWindow (log: LogEntry, limit: Limit, window: LimitWindow): Promise<boolean> {
+    // todo: check log against its limit and current limit_window
+    // handling rate vs threshold appropriately
+    return false
+  }
+
+  async updateLimitWindow (log: LogEntry, limit: Limit, window: LimitWindow) {
+    // todo: update limit_window in db for current log entry
+  }
+}
--- server/src/sendExceedanceNotification.ts
+++ server/src/sendExceedanceNotification.ts
@@ -1,6 +1,7 @@
+import { Resend } from 'resend'
+import { LimitManager } from './limitManager'
 import { LogEntry } from './saveLogRecord'
 
-// todo: need cleanup of old limit_windows
 export class SendExceedanceNotification {
   constructor (private readonly logEntry: LogEntry) {}
 
@@ -10,25 +11,34 @@ export class SendExceedanceNotification {
 
   private async checkPeriod () {
     const l = this.logEntry
+    const lm = new LimitManager(l)
+    const lim = await lm.getLimit(l)
+    const lw = await lm.getLimitWindow(l, lim)
 
-    // todo: make new LimitManager() with the following:
-    //   todo: function to call db limits table, returning limit info for current log
-    //   todo: function to update limit_window for current log entry as necessary
-    //   todo: function to check log against its limit and current limit_window
-    //          (handling rate vs threshold appropriately)
-
-    // todo: check log value against limitManager.checkLimit
-    // which if failed, will notify if limit condition met and
-    // limit hasn't yet been triggered for the period
-    if (!l.configID) {
+    if (await lm.checkLogLimitAndWindow(l, lim, lw)) {
       this.sendNotification()
     }
   }
 
   private async sendNotification () {
-    // get email address from .env -> EMAIL
-    // todo: send email to config value
-    //   notifying of limit exceeded as subject
-    //   body is subject again then log entry information
+    const email = process.env.EMAIL
+    if (!email) {
+      throw new Error('EMAIL not set in environment')
+    }
+
+    const resendKey = process.env.RESEND_API_KEY
+    if (!resendKey) {
+      throw new Error('RESEND_API_KEY not set in environment')
+    }
+
+    const resend: Resend = new Resend(resendKey)
+    resend.emails.send({
+      from: 'alerts@forfold.com',
+      to: email,
+      // todo: subject is limit exceeded string
+      subject: 'Hello World',
+      // todo: include subject string + log entry info
+      html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
+    })
   }
 }
